68785542ebdf80dea70eec9599a9b36ea456645c
sarthaxtic <128150513+sarthaxtic@users.noreply.github.com>
2025-05-20 23:10:10 +0530

    Update edsql_compiler.py

--- edsql_compiler.py
+++ edsql_compiler.py
@@ -2,13 +2,13 @@ import ply.lex as lex
 import ply.yacc as yacc
 import pandas as pd
 import matplotlib.pyplot as plt
-import spacy
 
 # ------------------ Lexical Analysis ------------------
 tokens = (
     'SELECT', 'FROM', 'WHERE', 'PLOT', 'BAR', 'GRAPH', 'LINE', 'PIE', 'CHART',
     'IDENTIFIER', 'NUMBER', 'STRING', 'COMMA', 'GREATER_THAN', 'EQUALS', 'SEMICOLON',
-    'LPAREN', 'RPAREN', 'AVG', 'GROUP', 'BY', 'PERFORMANCE_SCORE'
+    'LPAREN', 'RPAREN', 'AVG', 'GROUP', 'BY', 'ORDER', 'LIMIT', 'ASC', 'DESC',
+    'CUSTOM_METRIC'
 )
 
 reserved = {
@@ -16,7 +16,8 @@ reserved = {
     'PLOT': 'PLOT', 'BAR': 'BAR', 'GRAPH': 'GRAPH',
     'LINE': 'LINE', 'PIE': 'PIE', 'CHART': 'CHART',
     'AVG': 'AVG', 'GROUP': 'GROUP', 'BY': 'BY',
-    'PERFORMANCE_SCORE': 'PERFORMANCE_SCORE'
+    'ORDER': 'ORDER', 'LIMIT': 'LIMIT', 'ASC': 'ASC', 'DESC': 'DESC',
+    'CUSTOM_METRIC': 'CUSTOM_METRIC'
 }
 
 t_SELECT = r'SELECT'
@@ -31,7 +32,11 @@ t_CHART = r'CHART'
 t_AVG = r'AVG'
 t_GROUP = r'GROUP'
 t_BY = r'BY'
-t_PERFORMANCE_SCORE = r'PERFORMANCE_SCORE'
+t_ORDER = r'ORDER'
+t_LIMIT = r'LIMIT'
+t_ASC = r'ASC'
+t_DESC = r'DESC'
+t_CUSTOM_METRIC = r'CUSTOM_METRIC'
 t_COMMA = r','
 t_GREATER_THAN = r'>'
 t_EQUALS = r'='
@@ -62,23 +67,46 @@ def t_error(t):
 lexer = lex.lex()
 
 # ------------------ Parser ------------------
+
 def p_query(p):
-    'query : SELECT select_list FROM IDENTIFIER where_clause group_by_clause plot_clause SEMICOLON'
-    p[0] = ('QUERY', p[2], p[4], p[5], p[6], p[7])
+    '''query : SELECT select_list FROM IDENTIFIER where_clause group_by_clause plot_clause order_clause limit_clause SEMICOLON'''
+    p[0] = ('QUERY', p[2], p[4], p[5], p[6], p[7], p[8], p[9], p[10])
 
 def p_select_list(p):
-    '''select_list : IDENTIFIER COMMA select_list
-                   | IDENTIFIER
-                   | AVG LPAREN IDENTIFIER RPAREN
-                   | PERFORMANCE_SCORE LPAREN IDENTIFIER COMMA IDENTIFIER RPAREN'''
+    '''select_list : expression COMMA select_list
+                   | expression'''
     if len(p) == 4:
         p[0] = [p[1]] + p[3]
-    elif len(p) == 2:
+    else:
         p[0] = [p[1]]
-    elif len(p) == 5:
-        p[0] = [('AVG', p[3])]
-    elif len(p) == 7:
-        p[0] = [('CUSTOM_METRIC', p[1], p[3], p[5])]
+
+def p_expression(p):
+    '''expression : IDENTIFIER
+                  | function_call
+                  | avg_function
+                  | custom_metric'''
+    p[0] = p[1]
+
+def p_function_call(p):
+    '''function_call : IDENTIFIER LPAREN arg_list RPAREN'''
+    p[0] = ('FUNC_CALL', p[1], p[3])
+
+def p_avg_function(p):
+    '''avg_function : AVG LPAREN IDENTIFIER RPAREN'''
+    p[0] = ('AVG', p[3])
+
+def p_custom_metric(p):
+    '''custom_metric : CUSTOM_METRIC LPAREN IDENTIFIER COMMA arg_list RPAREN'''
+    metric_name = p[3].upper()  # Convert to uppercase string
+    p[0] = ('CUSTOM_METRIC', metric_name, *p[5])
+
+def p_arg_list(p):
+    '''arg_list : IDENTIFIER COMMA arg_list
+                | IDENTIFIER'''
+    if len(p) == 4:
+        p[0] = [p[1]] + p[3]
+    else:
+        p[0] = [p[1]]
 
 def p_where_clause(p):
     '''where_clause : WHERE condition
@@ -95,6 +123,27 @@ def p_group_by_clause(p):
                        | empty'''
     p[0] = ('GROUP BY', p[3]) if len(p) == 4 else None
 
+def p_order_clause(p):
+    '''order_clause : ORDER BY IDENTIFIER order_direction
+                    | empty'''
+    if len(p) == 5:
+        p[0] = ('ORDER BY', p[3], p[4])
+    else:
+        p[0] = None
+
+def p_order_direction(p):
+    '''order_direction : ASC
+                       | DESC'''
+    p[0] = p[1]
+
+def p_limit_clause(p):
+    '''limit_clause : LIMIT NUMBER
+                    | empty'''
+    if len(p) == 3:
+        p[0] = ('LIMIT', p[2])
+    else:
+        p[0] = None
+
 def p_plot_clause(p):
     '''plot_clause : PLOT BAR GRAPH
                    | PLOT LINE GRAPH
@@ -107,6 +156,10 @@ def p_empty(p):
     p[0] = None
 
 def p_error(p):
-    print(f"Syntax error at token {p.type} ('{p.value}')" if p else "Syntax error at EOF")
+    if p:
+        print(f"Syntax error at token {p.type} ('{p.value}')")
+    else:
+        print("Syntax error at EOF")
 
 parser = yacc.yacc()
+
